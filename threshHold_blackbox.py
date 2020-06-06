18c2b1f0d3b2236e6c1a4ead65fc1c8bd4a0d800
SrikarSiddarth <54308546+SrikarSiddarth@users.noreply.github.com>
2020-06-06 15:16:59 +0530

    Update threshHold_blackbox.py

--- threshHold_blackbox.py
+++ threshHold_blackbox.py
@@ -1,35 +1,8 @@
 import cv2 as cv
 import numpy as np
-import requests
-# if you are using ip webcam, then uncomment the following, other wise comment it
-# url= "http://192.168.1.3:8080/shot.jpg"
 
-# if you are using wired camera, uncomment the following, otherwise comment it
-cap = cv.VideoCapture(0)
-
-frame = None
-k = 0
-while(True):
-	# if you are using ip webcam, then uncomment the following, other wise comment it
-	# img_resp = requests.get(url)
-	# im_arr = np.array(bytearray(img_resp.content), dtype=np.uint8)
-	# frame = cv.imdecode(im_arr, -1)
-
-	# if you are using wired camera, uncomment the following as well as cap.release() found at the end of the code, otherwise comment it
-	ret, frame = cap.read()
-	if frame is not None and k==0:
-		img1 = frame
-		k=1
-		continue
-	elif k==1:
-		input('press any key to capture second image...')
-		img2 = frame
-		break
-
-# Our operations on the frame come here
-
-img1 = cv.resize(img1, (1080, 1080))
-img2 = cv.resize(img2, (1080, 1080))
+img1 = cv.imread('/home/srikarsiddarth/projects/garbageSegregator/a3.jpeg')
+img2 = cv.imread('/home/srikarsiddarth/projects/garbageSegregator/b3.jpeg')
 
 def preprocess(img):
 	gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
