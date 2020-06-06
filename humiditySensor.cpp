e662770cba49efa0a69be726585e5429dcc17279
SrikarSiddarth <54308546+SrikarSiddarth@users.noreply.github.com>
2020-06-06 17:37:24 +0530

    Update humiditySensor.cpp

--- humiditySensor.cpp
+++ humiditySensor.cpp
@@ -4,24 +4,30 @@
 #define dataPin 8               // Defines pin number to which the sensor is connected
 dht DHT;                        // Creats a DHT object
 std_msgs::UInt16 &humid_msg;
-ros::NodeHandle node_handle;    
+ros::NodeHandle node_handle;    // 
 ros::Publisher humidity_publisher("humidity", &humid_msg);
 void setup() {
   Serial.begin(9600);
   node_handle.advertise(humidity_publisher);
 }
 void loop() {
-  int readData = DHT.read22(dataPin);         // Reads the data from the sensor
-  //float t = DHT.temperature;                // Gets the values of the temperature
-  float h = DHT.humidity;                     // Gets the values of the humidity
-  humid_msg.data = h
-  // Printing the result on the serial monitor
+  int readData = DHT.read22(dataPin); // Reads the data from the sensor
+  //float t = DHT.temperature; // Gets the values of the temperature
+  float h = DHT.humidity; // Gets the values of the humidity
+  // Printing the results on the serial monitor
+  // Serial.print("Temperature = ");
+  // Serial.print(t);
+  // Serial.print(" *C ");
   Serial.print("    Humidity = ");
   Serial.print(h);
   Serial.println(" % ");
-
-  humidity_publisher.publish( &humid_msg );
-  node_handle.spinOnce();                     //handles communication
-  
-  delay(2000);                                // Delays 2 seconds, as the DHT22 so that the sampling rate is 0.5Hz
+  if (h>75){
+    humid_msg.data = 2
+  }
+  else {
+    humid_msg.data = 0
+  }
+  humidity_publisher.publish( &humid_msg);
+  node_handle.spinOnce();
+  delay(2000); // Delays 2 seconds, as the DHT22 so that the sampling rate is 0.5Hz
 }
